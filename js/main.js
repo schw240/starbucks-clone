257d5890b173fee9ea40f554321838ed5d1be17b
schw240 <schw240@gmail.com>
2021-07-01 13:06:46 +0900

    스크롤 위치 계산 애니메이션

--- js/main.js
+++ js/main.js
@@ -126,3 +126,16 @@ function floatingObject(selector, delay, size) {
 floatingObject('.floating1', 1, 15);
 floatingObject('.floating2',.5, 15);
 floatingObject('.floating3', 1.5, 20);
+
+
+const spyEls = document.querySelectorAll('section.scroll-spy')
+// 요소들 반복 처리
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({ // 감시할 장면(Scene)을 추가
+      triggerElement: spyEl, // 보여짐 여부를 감시할 요소를 지정
+      triggerHook: .8 // 화면의 80% 지점에서 보여짐 여부 감시
+    })
+    .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
+    .addTo(new ScrollMagic.Controller()) // 컨트롤러에 장면을 할당(필수!)
+})
